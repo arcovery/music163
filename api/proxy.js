99c9b909fc1a18c05b69926e87390141154381ec
arcovery <arcovery@qq.com>
2023-03-08 02:40:24 +0800

    11111111

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,20 @@
-// api/proxy.js
-// 该服务为 vercel serve跨域处理
-const { createProxyMiddleware } = require('http-proxy-middleware')
-
-module.exports = (req, res) => {
-	let target = ''
-	// 代理目标地址
-	// 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-	// target 替换为你跨域请求的服务器 如： http://baidu.com
-	if (req.url.startsWith('/backend')) {
-		target = 'https://music.zll.cool'
+const allowCors = fn => async (req, res) => {
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+	res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+	if (req.method === 'OPTIONS') {
+		res.status(200).end()
+		return
 	}
-	// 创建代理对象并转发请求
-	createProxyMiddleware({
-		target,
-		changeOrigin: true,
-		pathRewrite: {
-			// 通过路径重写，去除请求路径中的 `/backend`
-			// 例如 /backend/user/login 将被转发到 https://fanyi-api.baidu.com/user/login
-			'^/backend/': '/',
-		},
-	})(req, res)
+	return await fn(req, res)
 }
+
+const handler = (req, res) => {
+	const d = new Date()
+	res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
