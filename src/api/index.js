3121c5ab8aa6a594bd209e5587506e979f7d457d
arcovery <arcovery@qq.com>
2023-03-08 02:47:32 +0800

    1231

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 //基址
 const request = axios.create({
-	baseURL: 'https://music.zll.cool/backend',
+	baseURL: 'https://music.zll.cool',
 })
 //响应拦截器
 request.interceptors.response.use(
