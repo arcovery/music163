557351bac7929bd8475156d163b509c5d57ae81a
arcovery <arcovery@qq.com>
2023-03-08 01:26:18 +0800

    eeeeeee

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
 	// 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
 	// target 替换为你跨域请求的服务器 如： http://baidu.com
 	if (req.url.startsWith('/backend')) {
-		target = 'https://music.zll.cool'
+		target = 'http://music.zll.cool'
 	}
 	// 创建代理对象并转发请求
 	createProxyMiddleware({
