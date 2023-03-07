6ca88941206015102377da359fe5d960add9973f
arcovery <arcovery@qq.com>
2023-03-08 03:02:38 +0800

    222nnn

--- api/proxy.js
+++ api/proxy.js
@@ -5,6 +5,7 @@ const allowCors = fn => async (req, res) => {
 	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
 	res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+	res.setHeader('Content-Security-Policy: upgrade-insecure-requests')
 	if (req.method === 'OPTIONS') {
 		res.status(200).end()
 		return
