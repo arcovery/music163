7a4a33fe194e25dd09208581ea9402014123ed55
arcovery <arcovery@qq.com>
2023-03-08 11:29:25 +0800

    z

--- api/proxy.js
+++ api/proxy.js
@@ -3,12 +3,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
 	let target = ''
-
+console.log(req);
 	// 代理目标地址
 	// 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
 	// xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
 	if (req.url.startsWith('/backend')) {
-		target = 'http://music.zll.cool'
+		target = 'https://music.zll.cool'
 	}
 	// 创建代理对象并转发请求
 	createProxyMiddleware({
